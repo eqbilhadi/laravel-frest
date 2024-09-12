ce3bb50b5514c5bf764597c200c02b9d7730b19a
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-09-12 10:28:07 +0700

    Scripts and styles for layouts authentication

---
+++ resources/views/components/layouts/auth/includes/scripts.blade.php
@@ -0,0 +1,21 @@
+ <!-- / Content -->
+
+ <!-- Core JS -->
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+
+ <!-- Vendor JS -->
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+
+ <!-- Main JS -->
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
+
+ <!-- Page JS -->
+ <script src="{{ asset('assets/js/libs/jquery.js') }}"></script>
